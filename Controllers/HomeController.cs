778f3b213a2d48db2117b7f6e1d0f7e28261acbb
luoxudan <luoxudan@users.noreply.github.com>
2017-12-21 13:13:53 +0800

    Revert "try"
    This reverts commit 097130c22b34be5f2d9d752e199bce79e63f71b9.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,7 +12,6 @@ namespace Xmu.Crms.Web.ViceVersa.Controllers
             return Redirect("/Home/Login");
         }
 
-
         [Route("/Home/Login")]
         public IActionResult AccountLogin()
         {
