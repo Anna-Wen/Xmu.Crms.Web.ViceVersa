9a1b14613e9cf71e982c971d6b3cf898bf6707e4
luoxudan <luoxudan@users.noreply.github.com>
2017-12-24 17:07:44 +0800

    update

--- VO/ClassVO.cs
+++ VO/ClassVO.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Xmu.Crms.Shared.Models;
 
 namespace Xmu.Crms.Web.ViceVersa.VO
 {
@@ -12,8 +13,31 @@ namespace Xmu.Crms.Web.ViceVersa.VO
         public int NumStudent { get; set; }
         public string Time { get; set; }
         public string Site { get; set; }
-        public int Calling { get; set; }
         public string Roster { get; set; }
         public GradeProportionVO Proportions { get; set; }
+
+       
+        public ClassVO(long id, string name, int numStudent, string time, string site, string roster, GradeProportionVO proportions) 
+        {
+            Id = id;
+            Name = name;
+            NumStudent = numStudent;
+            Time = time;
+            Site = site;
+            Roster = roster;
+            Proportions = proportions;
+        }
+
+        public static implicit operator ClassVO(ClassInfo v)
+        {
+            int report = v.ReportPercentage == null ? 0:(int)v.ReportPercentage;
+            int present = v.PresentationPercentage == null ? 0 : (int)v.PresentationPercentage;
+            int three = v.ThreePointPercentage == null ? 0 : (int)v.ThreePointPercentage;
+            int four = v.FourPointPercentage == null ? 0 : (int)v.FourPointPercentage;
+            int five = v.FivePointPercentage == null ? 0 : (int)v.FivePointPercentage;
+            GradeProportionVO gradeProportionVO = new GradeProportionVO(report,present,three,four,five);
+            ClassVO classVO = new ClassVO(v.Id, v.Name, 0, v.ClassTime, v.Site,"学生名单",gradeProportionVO);
+            return classVO;
+        }
     }
 }
--- VO/GradeProportionVO.cs
+++ VO/GradeProportionVO.cs
@@ -7,6 +7,15 @@ namespace Xmu.Crms.Web.ViceVersa.VO
 {
     public class GradeProportionVO
     {
+        public GradeProportionVO(int report, int presentation, int c, int b, int a)
+        {
+            Report = report;
+            Presentation = presentation;
+            C = c;
+            B = b;
+            A = a;
+        }
+
         public int Report { get; set; }
         public int Presentation { get; set; }
         public int C { get; set; }
