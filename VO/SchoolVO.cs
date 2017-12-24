4774f9d1161be84d647f23ff5ba4d29b01116a90
Anna-Wen <Anna-Wen@users.noreply.github.com>
2017-12-24 16:57:20 +0800

    update on vo

--- VO/SchoolVO.cs
+++ VO/SchoolVO.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Xmu.Crms.Shared.Models;
 
 namespace Xmu.Crms.Web.ViceVersa.VO
 {
@@ -11,5 +12,19 @@ namespace Xmu.Crms.Web.ViceVersa.VO
         public string Name { get; set; }
         public string Province { get; set; }
         public string City { get; set; }
+
+        public SchoolVO(long id, string name, string province, string city)
+        {
+            Id = id;
+            Name = name;
+            Province = province;
+            City = city;
+        }
+
+        public static implicit operator SchoolVO(School v)
+        {
+            SchoolVO school = new SchoolVO(v.Id, v.Name, v.Province, v.City);
+            return school;
+        }
     }
 }
