e8d37f1afcfd4ac3838fba5fb611d3943f9dd6c1
Anna-Wen <Anna-Wen@users.noreply.github.com>
2017-12-25 16:11:18 +0800

    update on vo

--- VO/GroupVO.cs
+++ VO/GroupVO.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Xmu.Crms.Shared.Models;
 
 namespace Xmu.Crms.Web.ViceVersa.VO
 {
@@ -14,5 +15,13 @@ namespace Xmu.Crms.Web.ViceVersa.VO
         public List<TopicVO> Topics { get; set; }
         public string Report { get; set; }
         public SeminarGradeVO Grade { get; set; }
+
+        public static implicit operator GroupVO(SeminarGroup v)
+        {
+            // Group的Name命名方法还没有解决？？？
+            GroupVO groupVO = new GroupVO { Id = v.Id };
+
+            return groupVO;
+        }
     }
 }
