02f08a7d5352027046b238d0504e1c6ff53825a0
luoxudan <luoxudan@users.noreply.github.com>
2017-12-25 17:15:14 +0800

    update

--- VO/CourseClassVO.cs
+++ VO/CourseClassVO.cs
@@ -2,11 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Xmu.Crms.Shared.Models;
 
 namespace Xmu.Crms.Web.ViceVersa.VO
 {
     public class CourseClassVO
     {
+        public CourseClassVO(ClassInfo classInfo,int numstudent)
+        {
+            Id = classInfo.Id;
+            Name = classInfo.Name;
+            NumStudent = numstudent;
+            Time = classInfo.ClassTime;
+            Site = classInfo.Site;
+            CourseId = (int)classInfo.Course.Id;
+            CourseName = classInfo.Course.Name;
+            CourseTeacher = classInfo.Course.Teacher;
+        }
+
         public long Id { get; set; }
         public string Name { get; set; }
         public int NumStudent { get; set; }
